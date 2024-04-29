401e47915f475a205c91415ca1d89547c586d1f9
nguyennha <nguyennha1901@gmail.com>
2024-04-29 07:30:11 +0700

    updates

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+const core = require('@actions/core');
+const moment = require('moment');
+
+try {
+  const name = core.getInput('name');
+  console.log(`Hello, ${name}. We appreciate your business!`);
+  const timestamp = moment().format();
+  console.log(`Greeting issued at: ${timestamp}`);
+  core.setOutput("timestamp", timestamp);
+} catch (error) {
+  core.setFailed(error.message);
+}
